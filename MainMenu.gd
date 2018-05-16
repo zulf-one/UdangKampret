59e85bbbe9cc027edb1fe46917eea1c80cb1e85a
Zulf <zulfikar.assyura@gmail.com>
2018-05-16 21:37:56 +0700

    langkah setan

---
+++ MainMenu.gd
@@ -0,0 +1,44 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_bt_rules_pressed():
+	$pg_default.hide()
+	$pg_rules.show()
+	pass # replace with function body
+
+
+func _on_bt_score_pressed():
+	$pg_default.hide()
+	$pg_score.show()
+	pass # replace with function body
+
+
+func _on_bt_exit_pressed():
+	get_tree().quit()
+	pass # replace with function body
+
+
+func _on_bt_back_pressed():
+	$pg_rules.hide()
+	$pg_default.show()
+	pass # replace with function body
+
+
+func _on_bt_back2_pressed():
+	$pg_score.hide()
+	$pg_default.show()
+	pass # replace with function body
---
+++ Play.gd
@@ -0,0 +1,83 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Button_pressed():
+	$Button.hide()
+	$move.show()
+	pass # replace with function body
+
+
+func _on_01_pressed():
+	$move.hide()
+	$Button.show()
+	$map1.show()
+	pass # replace with function body
+
+
+func _on_02_pressed():
+	$move.hide()
+	$Button.show()
+	$map2.show()
+	pass # replace with function body
+
+
+func _on_03_pressed():
+	$move.hide()
+	$Button.show()
+	$map3.show()
+	pass # replace with function body
+
+
+func _on_04_pressed():
+	$move.hide()
+	$Button.show()
+	$map4.show()
+	pass # replace with function body
+
+
+func _on_05_pressed():
+	$move.hide()
+	$Button.show()
+	$map5.show()
+	pass # replace with function body
+
+
+func _on_06_pressed():
+	$move.hide()
+	$Button.show()
+	$map6.show()
+	pass # replace with function body
+
+
+func _on_07_pressed():
+	$move.hide()
+	$Button.show()
+	$map7.show()
+	pass # replace with function body
+
+
+func _on_08_pressed():
+	$move.hide()
+	$Button.show()
+	$map8.show()
+	pass # replace with function body
+
+
+func _on_09_pressed():
+	$move.hide()
+	$Button.show()
+	$map9.show()
+	pass # replace with function body
